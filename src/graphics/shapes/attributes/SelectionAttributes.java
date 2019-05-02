8204406b9cc954ce4f20cf584c1a0e69418f4262
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-05-02 16:32:56 +0200

    Update SelectionAttributes.java

--- src/graphics/shapes/attributes/SelectionAttributes.java
+++ src/graphics/shapes/attributes/SelectionAttributes.java
@@ -1,4 +1,38 @@
 package graphics.shapes.attributes;
 
-public class SelectionAttributes {
+public class SelectionAttributes extends Attributes 
+{
+	private String id;
+	private boolean selected;
+	
+	public SelectionAttributes()
+	{
+		this.id="Selection";
+		this.selected=false;
+	}
+	
+	public String getId() 
+	{
+		return this.id;
+	}
+	
+	public boolean isSelected()
+	{
+		return this.selected;
+	}
+	
+	public void select()
+	{
+		this.selected=true;
+	}
+	
+	public void unselect()
+	{
+		this.selected=false;
+	}
+	
+	public void toggleSelection()
+	{
+		this.selected=!this.selected;
+	}
 }
