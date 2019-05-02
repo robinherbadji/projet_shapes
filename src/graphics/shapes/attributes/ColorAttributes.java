8314a1d0acc38533644f02e9366f5e7d95084b69
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-05-02 16:31:39 +0200

    Update ColorAttributes.java

--- src/graphics/shapes/attributes/ColorAttributes.java
+++ src/graphics/shapes/attributes/ColorAttributes.java
@@ -1,4 +1,50 @@
 package graphics.shapes.attributes;
 
-public class ColorAttributes {
+import java.awt.Color;
+//import java.util.List;
+//import java.util.Vector;
+
+//import graphics.shapes.Shape;
+
+public class ColorAttributes extends Attributes
+{
+	private boolean filled;
+	private boolean strocked;
+	private Color filledColor;
+	private Color strockedColor;
+	private String id;
+	
+	public ColorAttributes(boolean filled, boolean strocked, Color filledColor, Color strockedColor)
+	{
+		this.id ="Color";
+		this.filled=filled;
+		this.strocked=strocked;
+		this.filledColor=filledColor;
+		this.strockedColor=strockedColor;
+	}
+	
+	public String getId() 
+	{
+		return this.id;
+	}
+	
+	public boolean filled()
+	{
+		return this.filled;	
+	}
+	
+	public boolean strocked()
+	{
+		return this.strocked;
+	}
+	
+	public Color filledColor()
+	{
+		return this.filledColor;
+	}
+	
+	public Color strockedColor()
+	{
+		return this.strockedColor;	
+	}
 }
