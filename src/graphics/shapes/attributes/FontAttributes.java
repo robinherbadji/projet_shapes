4ceb90d972bea5ee9adf7a3fd76f6027b9c01ad5
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-05-02 16:32:14 +0200

    Update FontAttributes.java

--- src/graphics/shapes/attributes/FontAttributes.java
+++ src/graphics/shapes/attributes/FontAttributes.java
@@ -1,4 +1,46 @@
 package graphics.shapes.attributes;
 
-public class FontAttributes {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Rectangle;
+
+public class FontAttributes extends Attributes 
+{
+	private Font font;
+	private Color fontColor;
+	private String id;
+	
+	public FontAttributes()
+	{
+		this(new Font("Arial",Font.PLAIN,12),Color.BLACK);
+	}
+	
+	public FontAttributes(Font font, Color fontColor)
+	{
+		this.id="Font";
+		this.font=font;
+		this.fontColor=fontColor;
+	}
+	
+	public String getId() 
+	{
+		return this.id;
+	}
+	
+	public Font font()
+	{
+		return this.font;
+	}
+	
+	public Color fontColor()
+	{
+		return this.fontColor;
+	}
+	
+	public Rectangle getBounds(String g)
+	{
+		Rectangle r=new Rectangle() ;
+		return r;
+	}
 }
+
