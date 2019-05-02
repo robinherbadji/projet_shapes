63447a848bcafcfd12ca2b5176b3329f9c024400
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-05-02 16:25:14 +0200

    Update Shape.java

--- src/graphics/shapes/Shape.java
+++ src/graphics/shapes/Shape.java
@@ -6,20 +6,24 @@ public abstract class Shape {
 	// Attributs
 	protected Point loc;
 	
-	//////////////////////////////////////////////////////////////////////////////////////////////
-	
-	// Constructeur
-	public Shape() {
-		// Vide pour l'instant, on verra après
+	public Attributes getAttributes(String s){
+		return this.attributes.get(s);
 	}
 	
 	//////////////////////////////////////////////////////////////////////////////////////////////
 	
-	// Méthodes (celles dont nous avons besoin pour l'instant sont toutes abstraites)
+	// Constructeur
+	public Shape() {
+		// Vide pour l'instant, on verra aprÃ¨s
+	}
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// MÃ©thodes (celles dont nous avons besoin pour l'instant sont toutes abstraites)
 	public abstract Point getLoc(); // Retourne la position de la forme (la variable loc)
 	
 	public abstract void setLoc(Point point); // Modifie la position de la forme (la variable loc)
-	
+	public abstract Rectangle getBounds();
 	public abstract void accept(ShapeVisitor sVisitor); // Permet d'accepter la visite du ShapeDraftman (dessinateur) chez une forme
 	
 }
