10b625d9554071b93ca3e0a845dc4b96a0b081fa
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-06-01 09:59:11 +0200

    Update Shape.java

--- src/graphics/shapes/Shape.java
+++ src/graphics/shapes/Shape.java
@@ -25,5 +25,6 @@ public abstract class Shape {
 	public abstract void setLoc(Point point); // Modifie la position de la forme (la variable loc)
 	public abstract Rectangle getBounds();
 	public abstract void accept(ShapeVisitor sVisitor); // Permet d'accepter la visite du ShapeDraftman (dessinateur) chez une forme
-	
+	public abstract boolean isText();
+    	public abstract void resize();
 }
