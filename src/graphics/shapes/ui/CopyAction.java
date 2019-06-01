f8142310e4c35a99a3087f1b3c1cb54e6fd2ab74
Camy00 <49454844+Camy00@users.noreply.github.com>
2019-06-01 09:41:20 +0200

    Create CopyAction.java

---
+++ src/graphics/shapes/ui/CopyAction.java
@@ -0,0 +1,32 @@
+package graphics.shapes.interpret;
+
+import java.awt.event.ActionEvent;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+import graphics.shapes.Shape;
+import graphics.shapes.ui.ShapeActionListener;
+import graphics.shapes.interpret.PasteAction;
+
+public class CopyAction extends ShapeActionListener {
+
+	private PasteAction paste ;
+	
+	public CopyAction(PasteAction pa) {
+		this.paste = pa ;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		List<Shape> shapes;
+		shapes=new Vector<Shape>();
+		for(Iterator<Shape> i=this.getModel().iterator(); i.hasNext();)
+		{
+			Shape s=(Shape)i.next();
+			if(s.getSelect().isSelected()) shapes.add((Shape)s);
+		}
+		this.paste.setCut(false);
+		this.paste.setShapes(shapes);
+	}
+}
